66eaf62092ed05a3560df80f269be83caef8618d
Michael <michael.hubbartt@gmail.com>
2024-12-28 16:22:53 -0600

    First Commit

---
+++ Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Copying C:\UnrealEngine 5.5\Engine\Binaries\Win64\ShaderCompileWorker.exe to C:\UnrealEngine 5.5\Engine\Binaries\Win64\XGEControlWorker.exe
+copy /Y /B "C:\UnrealEngine 5.5\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "C:\UnrealEngine 5.5\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
